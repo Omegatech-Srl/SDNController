739985fc87362d1f19f20e9dc1aad411386ffdf9
matteomegatech <152969673+matteomegatech@users.noreply.github.com>
2023-12-05 16:30:31 +0100

    Rimossa un po' di sporcizia

--- Omada.sh
+++ Omada.sh
@@ -15,9 +15,7 @@ elif [[ $OS = *"Ubuntu 18.04"* ]]; then
 elif [[ $OS = *"Ubuntu 20.04"* ]]; then
     OsVer=focal
 elif [[ $OS = *"Ubuntu 22.04"* ]]; then
-    # $OsVer is also set to 'focal' as MongoDB 4.4 is not offically supported on 22.04
     OsVer=focal
-    # Install libssl 1.1 as required by MongoDB 4.4 on Ubuntu 22.04
     echo "[+] Installazione libssl 1.1 per Ubuntu 22.04"
     wget -qP /tmp/ http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
     dpkg -i /tmp/libssl1.1_1.1.1f-1ubuntu2_amd64.deb &> /dev/null
@@ -34,7 +32,6 @@ echo "[+] Importazione MongoDB 4.4 PGP e creazione repo APT"
 curl -fsSL https://pgp.mongodb.com/server-4.4.asc | gpg -o /usr/share/keyrings/mongodb-server-4.4.gpg --dearmor
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-4.4.gpg ] https://repo.mongodb.org/apt/ubuntu $OsVer/mongodb-org/4.4 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.4.list
 
-# Package dependencies
 echo "[+] Installazione MongoDB 4.4"
 apt-get -qq update
 apt-get -qq install mongodb-org &> /dev/null
